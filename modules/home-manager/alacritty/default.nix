fcc3649bda2a244d73c4613e30ed8da917a6fd7a
meDracula <me.dracula@protonmail.com>
2025-06-21 18:01:32 +0200

    Alacritty nix config module added

---
+++ modules/home-manager/alacritty/default.nix
@@ -0,0 +1,12 @@
+########################
+# Home Manager Alacritty
+########################
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    alacritty
+  ];
+
+  xdg.configFile."alacritty/alacritty.toml".source = ./alacritty.toml;
+}
