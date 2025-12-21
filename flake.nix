694c9040b7014bbadf968d359be1603869536287
meDracula <me.dracula@protonmail.com>
2025-12-22 00:43:38 +0100

    Attempt to align home-manger with nixos channel version of 25.11

--- flake.nix
+++ flake.nix
@@ -6,11 +6,9 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
     # Home Manager
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
 
+    # Pre-commit Hook
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
