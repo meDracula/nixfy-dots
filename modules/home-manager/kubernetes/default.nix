8057521726304c694857e0037c5de84af7302ef7
meDracula <me.dracula@protonmail.com>
2025-12-22 00:28:26 +0100

    Home-manager kubernetes comment out (WIP) kubernetes config copy

--- modules/home-manager/kubernetes/default.nix
+++ modules/home-manager/kubernetes/default.nix
@@ -16,13 +16,14 @@ in
     argocd
   ];
 
-  home.file.".kube/config.src" = {
-    text = builtins.readFile kubeConfigFile;
-    onChange = ''
-      cat "${config.home.homeDirectory}/.kube/config.src" > "${config.home.homeDirectory}/.kube/config"
-      chmod 600 "${config.home.homeDirectory}/.kube/config"
-    '';
-  };
+  # WIP: The file kubeConfigFile must be secretly handled but requires read access for nixpkg (rething required!)
+  # home.file.".kube/config.src" = {
+  #   text = builtins.readFile kubeConfigFile;
+  #   onChange = ''
+  #     cat "${config.home.homeDirectory}/.kube/config.src" > "${config.home.homeDirectory}/.kube/config"
+  #     chmod 600 "${config.home.homeDirectory}/.kube/config"
+  #   '';
+  # };
 
   home.sessionVariables = {
     KUBECONFIG = "${config.home.homeDirectory}/.kube/config";
