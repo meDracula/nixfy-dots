6ebb1d29bbefb4f72cfbfde04934a257b4e2b87c
meDracula <me.dracula@protonmail.com>
2025-12-21 22:05:15 +0100

    Pre-commit moved into the flake.nix

--- flake.nix
+++ flake.nix
@@ -10,9 +10,11 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, home-manager }@inputs:
+  outputs = { self, nixpkgs, home-manager, pre-commit-hooks }@inputs:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
@@ -24,6 +26,30 @@
             # Required to install obsidian
             "electron-25.9.0"
           ];
+        };
+      };
+
+      preCommitCheck = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+
+        hooks = {
+          # --- formatting ---
+          nixfmt-rfc-style.enable = true;
+
+          # --- whitespace / files ---
+          trim-trailing-whitespace.enable = true;
+          end-of-file-fixer.enable = true;
+          check-executables-have-shebangs.enable = true;
+
+          # --- correctness ---
+          check-toml.enable = true;
+
+          # --- secret detection ---
+          ripsecrets.enable = true;
+          detect-private-keys.enable = true;
+
+          # --- shell ---
+          shellcheck.enable = true;
         };
       };
     in {
@@ -46,6 +72,26 @@
           modules = [ 
             ./users/victor/home-manager/home.nix
           ];
+        };
+      };
+
+      checks.${system}.pre-commit = preCommitCheck;
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          name = "Dev Station";
+          inputsFrom = [ preCommitCheck ];
+          packages = with pkgs; [
+            figlet
+            pre-commit
+            nixfmt-rfc-style
+            shellcheck
+          ];
+          shellHook = ''
+            ${preCommitCheck.shellHook}
+            echo "Dev Station" | figlet
+          '';
         };
       };
   };
