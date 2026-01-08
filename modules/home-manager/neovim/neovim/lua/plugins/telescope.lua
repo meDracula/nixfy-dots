fe14a4bd66d9c8223a4968cf5b7c99a6e5ca9440
meDracula <me.dracula@protonmail.com>
2026-01-08 18:16:11 +0100

    Telescope expand configurations options and move themes over to ivy

--- modules/home-manager/neovim/neovim/lua/plugins/telescope.lua
+++ modules/home-manager/neovim/neovim/lua/plugins/telescope.lua
@@ -9,13 +9,33 @@ require('telescope').setup {
       show_hidden = true,
       ignore = ".git",
     }
+  },
+  pickers = {
+    -- TODO { key = { theme = ivy } for key in [ find_files, command_history, buffers, lsp_references]}
+    find_files = {
+      theme = "ivy",
+    },
+    command_history = {
+      theme = "ivy",
+    },
+    buffers = {
+      theme = "ivy",
+    },
+    lsp_references = {
+      theme = "ivy",
+    }
   }
 }
 
 require('telescope').load_extension('fzf');
 local builtin = require('telescope.builtin');
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {});
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' });
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' });
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' });
+vim.keymap.set('n', '<leader>fh', builtin.command_history, { desc = 'Telescope command history' });
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'Telescope lsp references' });
 vim.keymap.set('n', '<leader>fs', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end);
+-- vim.keymap.set('n', '<leader>fh', builtin.commands, {});
