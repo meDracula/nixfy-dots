97a60475cc3e9027b1e2edfc84d447f583f31ee6
meDracula <me.dracula@protonmail.com>
2026-01-08 15:03:33 +0100

    Neovim Treesitter Config ensure_installed option not required for nix

--- modules/home-manager/neovim/neovim/lua/plugins/treesitter.lua
+++ modules/home-manager/neovim/neovim/lua/plugins/treesitter.lua
@@ -1,26 +1,5 @@
 -- TreeSitter
 require'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all"
-    ensure_installed = {
-	  "c",
-	  "go",
-	  "hcl",
-	  "javascript",
-	  "lua",
-	  "markdown",
-	  "markdown-inline",
-	  "nix",
-	  "python",
-	  "query",
-	  "rust",
-	  "sql",
-	  "terraform",
-	  "typescript",
-	  "vim",
-	  "vimdoc",
-	  "zsh",
-	},
-
     -- Automatically install missing parsers when entering buffer
     auto_install = false,
 
