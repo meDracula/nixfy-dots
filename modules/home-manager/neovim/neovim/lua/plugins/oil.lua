603e88f1697e923f0109bad2734a0a480ee78f6c
meDracula <me.dracula@protonmail.com>
2026-01-11 20:31:31 +0100

    Neovim fine tune Oil plugin options

--- modules/home-manager/neovim/neovim/lua/plugins/oil.lua
+++ modules/home-manager/neovim/neovim/lua/plugins/oil.lua
@@ -12,18 +12,13 @@ require("oil").setup({
   --  -- Configuration for the floating window in oil.open_float
   float = {
     -- Padding around the floating window
-    padding = 2,
+    padding = 1,
     -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-    -- max_width = 0,
-    -- max_height = 0,
+    max_width = 0.75,
+    max_height = 0.75,
     border = "rounded",
-    -- win_options = {
-    --   winblend = 0,
-    -- },
-    -- -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
-    -- get_win_title = nil,
-    -- -- preview_split: Split direction: "auto", "left", "right", "above", "below".
-    -- preview_split = "auto",
+    -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+    preview_split = "below",
     -- -- This is the config that will be passed to nvim_open_win.
     -- -- Change values here to customize the layout
     -- override = function(conf)
